d159b9c205ca8f8dc138e39a72781deae13ba5f1
yahyacodes <yahya.codin@gmail.com>
2023-09-15 15:51:01 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// name parameter function
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+// name & laguage parameters function
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// name & laguage parameters function
+function introductionWithLanguage(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
